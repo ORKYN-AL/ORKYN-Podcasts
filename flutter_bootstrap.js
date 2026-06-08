67979710acef4dc307f54137d863b0826d716c36
Il-Y-aN <ilyan.tajmouti@orkyn.fr>
2026-06-08 15:34:54 +0000

    Deploying to gh-pages from @ Il-Y-aN/ORKYN-Podcasts@3ced2f69c158cc834e7b2d80cdcf89b993d76d1c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1463126528" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2664007032" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
